6e0e4288f6ea4e8392d5a25cbfba75caa7009b44
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-03-14 16:12:05 +0600

    added react icon

--- src/component/home/CoreService.jsx
+++ src/component/home/CoreService.jsx
@@ -15,7 +15,7 @@ const CoreService = () => {
                     <div>
                         <div className="hero bg-base-200">
                             <div className="hero-content flex-col lg:flex-row">
-                                <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                                
                                 <div>
                                     <h1 className="text-5xl font-bold">Web Design</h1>
                                     <p className="py-6">The whole community is interested in growing the  FOX prize. The company is planningz</p>
