ecd7cabbb9af49e2a5c84a11f5e0c3682a39233f
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-03-14 13:46:17 +0600

    added target section

---
+++ src/component/home/Target.jsx
@@ -0,0 +1,48 @@
+import aboutImg from '../../assets/about_img.jpg'
+const Target = () => {
+    return (
+        <div className="max-w-[1300px] mx-auto px-10 ">
+            <div className='grid grid-cols-2 border-x'>
+                <div className='text-white'>
+                    <div role="tablist" className="tabs tabs-bordered">
+                        <input type="radio" name="my_tabs_1" role="tab" className="tab text-white " aria-label="OUR MISSION" />
+                        <div role="tabpanel" className="tab-content pt-20 space-y-5">
+
+                            <p> Our mission is to empower businesses and individuals by harnessing the full potential of the digital landscape. We are committed to delivering innovative web solutions that not only meet but exceed our clients' expectations. Through creativity, collaboration.</p>
+
+                            <p> Our vision is to be a trailblazing force in the world of web design and development, recognized for our unwavering commitment to excellence, integrity, and customer satisfaction.</p>
+
+                            <p>connecting businesses with their audiences, and individuals with their dreams. Our path forward is one of continuous growth</p>
+
+                        </div>
+
+                        <input type="radio" name="my_tabs_1" role="tab" className="tab text-white" aria-label="OUR VISSION" checked />
+                        <div role="tabpanel" className="tab-content pt-20 space-y-5">
+                            <p> Our mission is to empower businesses and individuals by harnessing the full potential of the digital landscape. We are committed to delivering innovative web solutions that not only meet but exceed our clients' expectations. Through creativity, collaboration.</p>
+
+                            
+
+                            <p>connecting businesses with their audiences, and individuals with their dreams. Our path forward is one of continuous growth</p>
+                        </div>
+
+                        <input type="radio" name="my_tabs_1" role="tab" className="tab text-white" aria-label="VALUE" />
+                        <div role="tabpanel" className="tab-content pt-20 space-y-5">
+                            <p> Our mission is to empower businesses and individuals by harnessing the full potential of the digital landscape. We are committed to delivering innovative web solutions that not only meet but exceed our clients' expectations. Through creativity, collaboration.</p>
+
+                            <p> Our vision is to be a trailblazing force in the world of web design and development, recognized for our unwavering commitment to excellence, integrity, and customer satisfaction.</p>
+
+                          
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src={aboutImg} alt="" className='bg-fixed
+                 filter grayscale '/>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Target;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import AboutSection from "../component/home/AboutSection";
 import Banner from "../component/home/Banner";
+import Target from "../component/home/Target";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             {/* <AboutSection></AboutSection> */}
+            <Target></Target>
         </div>
     );
 };
