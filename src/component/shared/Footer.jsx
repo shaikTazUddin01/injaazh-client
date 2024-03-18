bafcf24f8345940bc05e67445c9ada6da14d7e4a
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-03-18 23:49:16 +0600

    added ping us circle

--- src/component/shared/Footer.jsx
+++ src/component/shared/Footer.jsx
@@ -1,20 +1,29 @@
 import './Footer.css'
+import { BsArrowUpRight } from "react-icons/bs";
+
 
 const Footer = () => {
-    return (
-        <div className='footer_section'>
-           <div className="footer_greadint">
-           <div className='max-w-[1300px] mx-auto px-10'>
-                <div className='border-x '>
-                   <div className='min-h-[700px]'>
-
-                   </div>
-                </div>
+   return (
+      <div className='footer_section'>
+         <div className="footer_greadint">
+            <div className='max-w-[1300px] mx-auto px-10'>
+               <div className='border-x '>
+                  <div className='min-h-[700px] flex justify-between items-center'>
+                     <div>
+                        <div className='ping_us flex justify-center items-center flex-col'>
+                           <BsArrowUpRight className='text-5xl'></BsArrowUpRight>
+                           <h1 className='text-6xl uppercase font-bold'>Ping us</h1>
+                        </div>
+                     </div>
+                     <div></div>
+                     <div></div>
+                  </div>
+               </div>
 
             </div>
-           </div>
-        </div>
-    );
+         </div>
+      </div>
+   );
 };
 
 export default Footer;
