7d152387fcb91b838d27cfc6dd7a009e86cd51e7
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-03-18 23:53:45 +0600

    added ping us hover animation

--- src/component/shared/Footer.jsx
+++ src/component/shared/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <div className='max-w-[1300px] mx-auto px-10'>
                <div className='border-x '>
                   <div className='min-h-[700px] flex justify-between items-center'>
-                     <div>
+                     <div className='w-[500px] h-[500px] flex justify-center items-center'>
                         <div className='ping_us flex justify-center items-center flex-col'>
                            <BsArrowUpRight className='text-5xl'></BsArrowUpRight>
                            <h1 className='text-6xl uppercase font-bold'>Ping us</h1>
